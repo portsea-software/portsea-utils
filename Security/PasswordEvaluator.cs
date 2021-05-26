e4fe73fbf5a0e54513e8237ac408f853b5a3bfa7
Malcolm <malcolm@portseasoftware.com>
2021-05-26 16:43:38 +0100

    Added Password Evaluator class.

---
+++ Security/PasswordEvaluator.cs
@@ -0,0 +1,82 @@
+﻿namespace Portsea.Utils.Security
+{
+    public class PasswordEvaluator
+    {
+        private readonly int passwordLength;
+
+        private readonly PasswordRules passwordRules;
+
+        private int digitCount;
+
+        private int uppercaseCharacterCount;
+
+        private int lowercaseCharacterCount;
+
+        private int specialCharacterCount;
+
+        public PasswordEvaluator(string password, PasswordRules passwordRules)
+        {
+            this.passwordRules = passwordRules;
+            password = password.Trim();
+            this.passwordLength = password.Length;
+            foreach (var character in password)
+            {
+                this.EvaluateCharacter(character);
+            }
+        }
+
+        public bool StrongEnough()
+        {
+            return this.CheckMinimumLength() &&
+                this.CheckLowercaseCharacters() &&
+                this.CheckUppercaseCharacters() &&
+                this.CheckDigits() &&
+                this.CheckSpecialCharacters();
+        }
+
+        private void EvaluateCharacter(char character)
+        {
+            if (char.IsDigit(character))
+            {
+                this.digitCount++;
+            }
+            else if (char.IsUpper(character))
+            {
+                this.uppercaseCharacterCount++;
+            }
+            else if (char.IsLower(character))
+            {
+                this.lowercaseCharacterCount++;
+            }
+            else
+            {
+                this.specialCharacterCount++;
+            }
+        }
+
+        private bool CheckMinimumLength()
+        {
+            return this.passwordLength >= this.passwordRules.MinimumLength;
+        }
+
+        private bool CheckLowercaseCharacters()
+        {
+            return this.lowercaseCharacterCount >= this.passwordRules.MinimumLowercaseCharacters;
+        }
+
+        private bool CheckUppercaseCharacters()
+        {
+            return this.uppercaseCharacterCount >= this.passwordRules.MinimumUppercaseCharacters;
+        }
+
+        private bool CheckDigits()
+        {
+            return this.digitCount >= this.passwordRules.MinimumDigits;
+        }
+
+        private bool CheckSpecialCharacters()
+        {
+            return this.specialCharacterCount >= this.passwordRules.MinimumSpecialCharacters;
+        }
+    }
+}
---
+++ Security/PasswordRules.cs
@@ -0,0 +1,15 @@
+﻿namespace Portsea.Utils.Security
+{
+    public class PasswordRules
+    {
+        public int MinimumLength { get; set; }
+
+        public int MinimumLowercaseCharacters { get; set; }
+
+        public int MinimumUppercaseCharacters { get; set; }
+
+        public int MinimumDigits { get; set; }
+
+        public int MinimumSpecialCharacters { get; set; }
+    }
+}
