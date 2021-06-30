4243f526e3a02d5485b9094600c8bfd688aa35ea
Malcolm <malcolm@portseasoftware.com>
2021-06-30 17:38:02 +0100

    Added converter class for converting from strings to value types.

---
+++ Portsea.Utils/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Portsea.Utils
+{
+    public static class Converter
+    {
+        public static T Convert<T>(string value)
+            where T : struct
+        {
+            TypeConverter typeConverter = TypeDescriptor.GetConverter(typeof(T));
+            return (T)typeConverter.ConvertFrom(value);
+        }
+    }
+}
