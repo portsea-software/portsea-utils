d7e9a564d29c08576eb80da4d00cc7121b1028e1
Malcolm <malcolm@portseasoftware.com>
2021-10-08 17:20:48 +0100

    Added EnumExtensions class.

---
+++ Portsea.Utils/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Portsea.Utils
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<Enum> GetFlags(this Enum type)
+        {
+            foreach (Enum value in Enum.GetValues(type.GetType()))
+            {
+                if (type.HasFlag(value))
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return (T[])Enum.GetValues(typeof(T));
+        }
+
+        public static IEnumerable<string> GetNames<T>()
+        {
+            return Enum.GetNames(typeof(T));
+        }
+    }
+}
