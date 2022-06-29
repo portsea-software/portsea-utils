8accb921d94dcc0dacd91ad64d85c307ed0ae8b3
Malcolm <malcolm@portseasoftware.com>
2022-06-29 13:18:03 +0100

    Added Email Validator

---
+++ Portsea.Utils.Tests/Validation/EmailValidatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Portsea.Utils.Validation;
+using NUnit.Framework;
+
+namespace Portsea.Utils.Tests.Validation
+{
+    public class EmailValidatorTests
+    {
+        [TestCase("example@example.com", true)]
+        [TestCase("a@a.d", true)]
+        [TestCase("false@true", false)]
+        [TestCase("notanemailaddress", false)]
+        public void Is_Valid_Email_Address(string emailAddress, bool expectedResult)
+        {
+            // Act
+            bool result = EmailValidator.IsValid(emailAddress);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
---
+++ Portsea.Utils/Validation/EmailValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Portsea.Utils.Validation
+{
+    public static class EmailValidator
+    {
+        private static readonly Regex RegEx = CreateRegEx();
+
+        public static bool IsValid(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+
+            return RegEx.IsMatch(value);
+        }
+
+        // NOTE: this method is adapted from the .NET reference source code
+        // SEE: https://github.com/microsoft/referencesource/blob/master/System.ComponentModel.DataAnnotations/DataAnnotations/EmailAddressAttribute.cs
+        private static Regex CreateRegEx()
+        {
+            const string pattern = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+            const RegexOptions options = RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture;
+            return new Regex(pattern, options);
+        }
+    }
+}
