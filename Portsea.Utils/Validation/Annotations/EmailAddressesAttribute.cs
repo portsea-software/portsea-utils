0e7150df64e648df496e6a54c3c55743d98388de
Malcolm <malcolm@portseasoftware.com>
2023-05-23 17:26:46 +0100

    Added EmailAddresses attribute and FilePaths attribute.

---
+++ Portsea.Utils/Validation/Annotations/EmailAddressesAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portsea.Utils.Validation.Annotations
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class EmailAddressesAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            this.ErrorMessage = string.Empty;
+
+            // Email Address cannot be null
+            if (value != null)
+            {
+                if (value is IEnumerable<string>)
+                {
+                    IEnumerable<string> emailAddresses = value as IEnumerable<string>;
+                    foreach (string emailAddress in emailAddresses)
+                    {
+                        if (!EmailValidator.IsValid(emailAddress))
+                        {
+                            this.ErrorMessage += $"{emailAddress} is not a valid email address.\r\n";
+                        }
+                    }
+                }
+                else
+                {
+                    this.ErrorMessage =
+                        $"Email Addresses should be specified as IEnumerable<string>.  Object passed is of type {value.GetType().FullName}";
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.ErrorMessage))
+            {
+                return new ValidationResult(this.ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Portsea.Utils/Validation/Annotations/FilePathsAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Portsea.Utils.Validation.Annotations
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class FilePathsAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            this.ErrorMessage = string.Empty;
+
+            if (value != null)
+            {
+                if (value is IEnumerable<string>)
+                {
+                    IEnumerable<string> files = value as IEnumerable<string>;
+
+                    foreach (string file in files)
+                    {
+                        if (string.IsNullOrWhiteSpace(file) || !System.IO.File.Exists(file))
+                        {
+                            this.ErrorMessage += $"File does not exist: {file}\r\n";
+                        }
+                    }
+                }
+                else
+                {
+                    this.ErrorMessage =
+                        $"File List should be specified as IEnumerable<string>.  Object passed is of type {value.GetType().FullName}";
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.ErrorMessage))
+            {
+                return new ValidationResult(this.ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
